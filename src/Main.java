9b36942f61eec3be2a2d71c39b29eda95290e62c
AbrarTaseen <abrart4@nycstudents.net>
2025-10-24 11:48:36 -0400

    Implement isLeapYear and numberOfLeapYears

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(APCalendar.numberOfLeapYears(2020, 2024));
+        System.out.println(APCalendar.numberOfLeapYears(2021, 2024));
+        System.out.println(APCalendar.numberOfLeapYears(2020, 2025));
+    }
 }
